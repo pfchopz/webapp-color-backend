3edf4800cae07682059c2352382672f2ce0ae2cd
root <root@DESKTOP-VDA9MIF.localdomain>
2022-05-15 18:16:50 -0500

    Added support for DB SERVER IP as env variable

--- backend.py
+++ backend.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
+import os
 
 # Create FastAPI object
 app = FastAPI()
 
+# Get Database Server IP as Environment Variable
+DB_SERVER_IP = os.environ.get('DB_SERVER_IP')
+
 # Create PostGres DB connection and pull message data
-engine = create_engine("postgresql://dan:password@localhost:5432/postgres")
+engine = create_engine(f"postgresql://dan:password@{DB_SERVER_IP}:5432/postgres")
 with engine.connect() as conn:
     data = conn.execute("SELECT message FROM data")
     for entry in data:
