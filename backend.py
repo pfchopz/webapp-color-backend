c4fad68d9ead82ce20ddeef34d33c8ed747e7531
root <root@DESKTOP-VDA9MIF.localdomain>
2022-05-15 18:11:23 -0500

    added database functions

--- backend.py
+++ backend.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
 
+# Create FastAPI object
 app = FastAPI()
 
+# Create PostGres DB connection and pull message data
+engine = create_engine("postgresql://dan:password@localhost:5432/postgres")
+with engine.connect() as conn:
+    data = conn.execute("SELECT message FROM data")
+    for entry in data:
+        dbdata = entry
 
 @app.get("/message")
 async def root():
-    return {"message": "Hello World"}
+    return dbdata['message']
