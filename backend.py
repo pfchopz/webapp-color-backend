fe95464bfd88d550b31cb3f064ea34f83c8eaaa1
root <root@DESKTOP-VDA9MIF.localdomain>
2022-05-15 22:22:41 -0500

    Added more env for user and pass

--- backend.py
+++ backend.py
@@ -6,10 +6,12 @@ import os
 app = FastAPI()
 
 # Get Database Server IP as Environment Variable
-DB_SERVER_IP = os.environ.get('DB_SERVER_IP')
+DB_SERVER= os.environ.get('DB_SERVER')
+DB_USER= os.environ.get('DB_USER')
+DB_PASS= os.environ.get('DB_PASS')
 
 # Create PostGres DB connection and pull message data
-engine = create_engine(f"postgresql://dan:password@{DB_SERVER_IP}:5432/postgres")
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASS}@{DB_SERVER}:5432/postgres")
 with engine.connect() as conn:
     data = conn.execute("SELECT message FROM data")
     for entry in data:
