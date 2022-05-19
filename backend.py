be787b6695429a8319b4f8396b782292b10da8f5
root <root@DESKTOP-VDA9MIF.localdomain>
2022-05-19 18:54:11 -0500

    Modified SQL code

--- backend.py
+++ backend.py
@@ -20,8 +20,8 @@ with engine.connect() as conn:
 @app.get("/message")
 async def root():
     with engine.connect() as conn:
-        data = conn.execute("SELECT message FROM data")
+        data = conn.execute("SELECT messages FROM data")
         for entry in data:
             dbdata = entry
-            
+
     return dbdata['message']
